e9b9ac54846ca59725497efd42dbd7eb56f40d21
getrolucas <getrolucasfilho@gmail.com>
2023-11-22 20:08:14 -0300

    update geral

--- app.py
+++ app.py
@@ -1,45 +1,70 @@
 import streamlit as st
 import pandas as pd
-import csv
 import time
 from streamlit_gsheets import GSheetsConnection
 
-# Create a connection object.
+# criando conexão ao "banco"
 conn = st.connection("gsheets", type=GSheetsConnection)
 
-df = conn.read()
+# query
+sql = '''
+SELECT 
+    "nome", 
+    "item" 
+FROM
+    dados
+WHERE
+   "item" IS NOT NULL
+'''
 
-st.write(df)
+# lendo dados do "banco"
+df_selecionados = conn.query(sql=sql)
 
-# Interface do Streamlit
+# dados estaticos
+df_lista = pd.read_csv("lista.csv")
+
+# quantidade atual de cada item selecionado
+atual = df_selecionados["item"].value_counts()
+
+# função para limpar itens já selecionados
+def ajustar_quantidades(i):
+    if i['item'] in atual:
+        return i['quantidade'] - atual[i['item']]
+    else:
+        return i['quantidade']
+
+# Aplicando a função ao primeiro DataFrame
+df_lista['quantidade'] = df_lista.apply(ajustar_quantidades, axis=1)
+
+# dados para selectbox
+presentes = df_lista[df_lista["quantidade"] > 0]["item"].to_list()
+
+# interface do Streamlit
 st.title(":sun_with_face: :hibiscus: :gray[Chá da Luísa] :sunflower: :blossom:")
 
-'''
-# Seleção dos valores para as duas colunas
+# seleção dos valores para as duas colunas
 st.subheader("Escreva seu nome")
 nome = st.text_input('',None, placeholder="Obrigatório")
 
 st.subheader("Selecione um presentinho")
-selec_presentes = st.selectbox('', presentes, placeholder="Escolha uma opção")
-
-# Função para escrever no arquivo CSV
-def salvar_dados(nome, presente):
-    with open("selecionados.csv", "a", newline="") as arquivo_csv:
-        writer = csv.writer(arquivo_csv)
-        writer.writerow([nome, presente])
+item_selecionado = st.selectbox('', presentes, placeholder="Escolha uma opção")
 
 # Botão para salvar
 if st.button("Enviar"):
     if nome:
-        salvar_dados(nome, selec_presentes)
-        st.success(f"{nome}, suas opção {selec_presentes} foi enviada para os papais")
-        lista.loc[selec_presentes, "quantidade"] = lista.loc[selec_presentes, "quantidade"] - 1
-        lista.to_csv("lista.csv")
+        dados = pd.DataFrame({"nome":[nome], "item":[item_selecionado]})
+
+        st.write(dados)
+        data_update = pd.concat((df_selecionados, dados), ignore_index=True)
+        st.write(data_update)
+        conn.update(data=data_update)
         
-        time.sleep(3)
+        st.success(f"{nome}, sua opção {item_selecionado} foi enviada para os papais")
+    
+        time.sleep(5)
+        
+        st.cache_data.clear()
         st.rerun()
         
     else:
         st.warning("Insira seu nome", icon="⚠️")
-
-'''
