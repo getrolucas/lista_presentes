7595e78cd04da3e5d2e38757b04b9972a541d679
getrolucas <getrolucasfilho@gmail.com>
2023-11-23 22:56:44 -0300

    att app confirmacao envio

--- app.py
+++ app.py
@@ -63,16 +63,25 @@ if st.button("Enviar"):
         st.warning("Escolha uma opção de presente", icon="⚠️")
     else:  
         if nome:
+            
             dados = pd.DataFrame({"nome":[nome], "item":[item_selecionado]})
             data_update = pd.concat((df_selecionados, dados), ignore_index=True)
             conn.update(data=data_update)
+            
+            progress_text = "Enviando informação."
+            
+            my_bar = st.progress(0, text=progress_text)
 
+            for percent_complete in range(100):
+                time.sleep(0.01)
+                my_bar.progress(percent_complete + 1, text=progress_text)
+            time.sleep(1)
+            my_bar.empty()
+            
+            st.balloons()
+            
             st.success(
                 f"{nome}, sua opção {item_selecionado} foi enviada para os papais. Aguardamos você!")
-        
-            time.sleep(5)
-            st.cache_data.clear()
-            st.rerun()
             
         else:
             st.warning("Insira seu nome", icon="⚠️")
